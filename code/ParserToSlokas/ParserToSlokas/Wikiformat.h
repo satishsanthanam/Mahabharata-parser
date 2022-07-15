e2bdbaa0e7a17db0aa992c732f84db8d22eadd63
satish santhanam <satishsanthanam@gmail.com>
2022-07-15 20:10:03 +0530

    Code and Formatted Slokas of Mahabarata

---
+++ code/ParserToSlokas/ParserToSlokas/Wikiformat.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <codecvt>
+#include "MTP.h"
+
+class Wikiformat
+{
+private:
+	void wikicode();
+};
+
